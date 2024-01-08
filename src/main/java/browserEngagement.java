cf42656ae368b46294f7c3ea733dd5eabf90130d
TommyBerman <tomy.berman1@gmail.com>
2024-01-08 14:50:58 +0200

    changed code in browser engagement to selenium

--- src/main/java/browserEngagement.java
+++ src/main/java/browserEngagement.java
@@ -1,15 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import java.awt.Desktop;
 import java.net.URI;
 
 
 public class browserEngagement {
+    public static WebDriver driver;
 
     public static void main(String[] args) throws Exception {
-        Desktop desk = Desktop.getDesktop();
-        desk.browse(new URI("https://www.example.com"));
+//        Desktop desk = Desktop.getDesktop();
+//        desk.browse(new URI("https://www.example.com"));
 
+        driver = new ChromeDriver();
+        driver.get("https://www.example.com");
 
-        // I don't know how to extract title with only using java
-        System.out.println();
+        System.out.println(driver.getTitle());
     }
 }
